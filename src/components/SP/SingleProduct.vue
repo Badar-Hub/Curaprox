3690b21c69e3be0f88ecc9265c359596fa015e81
Badar Abdullah <bapk65@gmail.com>
2020-11-24 08:57:09 +0500

    refactor code

--- src/components/SP/SingleProduct.vue
+++ src/components/SP/SingleProduct.vue
@@ -5,65 +5,31 @@
         <img :src="`${(!isDev? '/admin/' : '' ) + img}`" />
       </div>
       <hr />
-      <div class="text">
-        <h2>{{name}}</h2>
-        <h3>RS: {{price}}</h3>
-        <!-- <p>{{shortDesc}}</p> -->
-      </div>
+      <h2>{{name}}</h2>
+      <h3>RS: {{price}}</h3>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      isDev: false,
-    };
-  },
-  mounted() {
-    this.isDev = process.env.NODE_ENV === "development";
-  },
   props: {
-    _id: String,
+    img: String,
     name: String,
-    price: Number,
-    // shortDesc: { type: String, required: false },
-    img: { type: String, required: false },
+    price: String,
   },
 };
 </script>
 
-<style scoped lang="sass">
-.prodmain
-  padding: 0.8rem
-.singleProduct
-  display: flex
-  flex-wrap: wrap
-  flex-direction: column
-  max-width: 240px
-  width: auto
-  padding-right: 15px
-  transition: 0.2s
-  box-shadow: 0px 0px 5px 0px rgba(207,201,201,1)
-  -webkit-box-shadow: 0px 0px 5px 0px rgba(207,201,201,1)
-  -moz-box-shadow: 0px 0px 5px 0px rgba(207,201,201,1)
-  cursor: pointer
-  &:hover
-    box-shadow: 0px 0px 5px 3px rgba(204,198,198,1)
-    -webkit-box-shadow: 0px 0px 5px 3px rgba(204,198,198,1)
-    -moz-box-shadow: 0px 0px 5px 3px rgba(204,198,198,1)
-  .img
-    width: 240px
-    max-hight: 300px
-    height: auto
-    padding: 20px
-  img
-    max-width: 240px
-    max-height: 300px
-    width: auto
+<style>
+.prodmain {
+}
 
-  .text
-    margin: auto
-    text-align: left
+.single-product {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  max-width: 400px;
+  width: auto;
+}
 </style>
